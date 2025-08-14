bf34c489b4fd1f6067e2f8297a85ce3e0fcaf554
Malcolm Odhiambo Awuor <vincentmalcom64@gmail.com>
2025-08-14 21:17:47 +0300

    Add Header, MainContent, and Footer components and integrate into App

---
+++ alx-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header style={styles.header}>
+      <h1>My Favorite Cities 🚀</h1>
+    </header>
+  );
+}
+
+const styles = {
+  header: {
+    backgroundColor: '#282c34',
+    color: 'white',
+    padding: '1rem',
+    textAlign: 'center',
+  }
+};
+
+export default Header;
