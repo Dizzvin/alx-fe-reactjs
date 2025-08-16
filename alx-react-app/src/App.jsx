228d38fda4787a09d624cb47428395d96fada122
Malcolm Odhiambo Awuor <vincentmalcom64@gmail.com>
2025-08-16 20:26:56 +0300

    Initial React app setup with Vite

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
-
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <welcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
