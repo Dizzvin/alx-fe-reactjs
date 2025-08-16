e624b37d74420c2dd1e095b2c9984211cb093830
Malcolm Odhiambo Awuor <vincentmalcom64@gmail.com>
2025-08-17 01:58:19 +0300

    Add UserProfile component

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,12 @@
+const UserProfile = ({ name, age, bio }) => {
+  return (
+    <div>
+      <h2>{name}MalcoLm Odhiambo</h2>
+      <p>Age: {28}</p>
+      <p>Bio: {Passionate about technology, always eager to learn new skills, and enjoys solving challenging problems}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
+
