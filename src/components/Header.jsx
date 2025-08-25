c8c70c42cb2335d4dd7c3bc117bdf5a46d58ee20
Malcolm Odhiambo Awuor <vincentmalcom64@gmail.com>
2025-08-25 19:36:59 +0300

    Fix Header component and clean up old files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 function Header() {
   return (
-    <header style={{ backgroundColor: 'navy', color: 'mhite', textAlign: 'center', padding: '20x' }}>
-      <h1>My Favorite Cities</h1>
+   <header 
+     style={{
+      backgroundColor: '#4CAF50',
+      color: 'white',
+      padding: '15px',
+      textAlign: 'center',
+      fontSize: '24px'
+    }}
+  >
+     <h1 style={{ margin: 0, letterSpacing: '0.5px' }}>
+       My Favorite Cities
+     </h1>
     </header>
   );
 }
 
 export default Header;
-
