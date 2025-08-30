203e583fb2e66f7cec2b518cd955f5a0ce66cee7
Malcolm Odhiambo Awuor <vincentmalcom64@gmail.com>
2025-08-30 13:00:19 +0300

    change colour to blue and capitalize C in counter component

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -9,7 +9,7 @@ function UserProfile(props) {
         backgroundColor: "#f9f9f9",
       }}
     >
-      <h2 style={{ color: "yellow", marginBottom: "10px" }}>{props.name}</h2>
+      <h2 style={{ color: "blue", marginBottom: "10px" }}>{props.name}</h2>
       <p>
         Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
       </p>
